e877949b933988d812523bfc79f9e0ea183af268
Andrej A Antonov <polymorphm@gmail.com>
2013-01-03 06:01:08 +0400

    created ``AboutWindow._open_web_page``

--- lib_verif_scheduler_rf_gui_2013_01_02/about_window.py
+++ lib_verif_scheduler_rf_gui_2013_01_02/about_window.py
@@ -17,13 +17,15 @@
 
 assert str is not bytes
 
+import subprocess
 import tkinter
 from tkinter import ttk, scrolledtext
+from . import tk_mt
+
+WEB_PAGE = 'https://github.com/verif-scheduler-2012-12-25'
 
 ABOUT_TEXT = """\
 Copyright 2012, 2013 Andrej A Antonov <polymorphm@gmail.com>.
-
-Web page: https://github.com/verif-scheduler-2012-12-25 .
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
@@ -41,15 +43,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 class AboutWindow:
     def __init__(self):
         self._root = tkinter.Tk()
+        self._tk_mt = tk_mt.TkMt(self._root)
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
+        
         self._root.title(string='О Программе "Планирование проверок"')
         
         self._text = scrolledtext.ScrolledText(master=self._root)
         self._text.insert(tkinter.END, ABOUT_TEXT)
         self._text.config(state=tkinter.DISABLED)
         
-        self._close_button = ttk.Button(master=self._root, text='Закрыть',
-                command=self._root.destroy)
+        self._web_button = ttk.Button(master=self._root,
+                text='Открыть web-страницу',
+                command=self._open_web_page)
+        self._close_button = ttk.Button(master=self._root,
+                text='Закрыть окно',
+                command=self.close)
         
         self._text.pack(fill=tkinter.BOTH, expand=True)
+        self._web_button.pack()
+        self._close_button.pack()
+    
+    def close(self):
+        self._tk_mt.push_destroy()
+    
+    def _open_web_page(self):
+        def thread_target():
+            subprocess.call(('xdg-open', WEB_PAGE))
         
-        self._close_button.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
+        self._tk_mt.start_daemon(thread_target)
